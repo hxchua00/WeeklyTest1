d0512811eb556ba8cbbb68014f18799766662648
hxchua00 <hxchua00@gmail.com>
2021-09-20 18:07:36 +0800

    Add project files.

---
+++ WeeklyExamQ2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyExamQ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "I love programming very much";
+
+            char[] arr = new char[message.Length];
+
+            for(int i=0; i < arr.Length; i++)
+            {
+                arr[i] = message[message.Length - 1];
+            }
+
+        }
+    }
+}
---
+++ WeeklyTest1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyExamQ1
+{
+    class WeeklyExamQ1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array size: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+
+            Random rnd = new Random();
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(0, 3);
+            }
+
+            Array.Sort(arr);
+
+            Console.WriteLine();
+            foreach(int x in arr)
+            {
+                Console.Write(x + "");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
